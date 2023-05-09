198ed06630179e067a4949042991efd596742a8d
김태형 <gimtaehyeong@gimtaehyeong-ui-MacBookAir.local>
2023-05-09 21:52:12 +0900

    0509

---
+++ Vue_Training/과제3_4/src/views/First.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="First">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="첫 번째 페이지입니다"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld.vue'
+export default {
+  name: 'FirstVue',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ Vue_Training/과제3_4/src/views/Second.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="Second">
+    <h1>두 번쨰 페이지입니다</h1>
+  </div>
+</template>
+<script>
+export default{
+  name: 'SecondVue',
+}
+</script>
