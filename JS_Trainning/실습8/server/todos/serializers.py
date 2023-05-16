d875c51818cb8a7e9e983cca31dfa1e72538b3ae
김태형 <gimtaehyeong@gimtaehyeong-ui-MacBookAir.local>
2023-05-16 22:12:01 +0900

    0516

--- JS_Trainning/실습8/server/todos/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Todo
-
-
-class TodoSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source='user.username', read_only=True)
-    class Meta:
-        model = Todo
-        fields = ('id', 'title', 'is_completed', 'user', 'username')
